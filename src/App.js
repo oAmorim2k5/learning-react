871347ce8941356111593bf76bae5d2ec07bf35b
oAmorim2k5 <victorhuski@gmail.com>
2024-09-15 17:44:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  const name = 'Victor'
+  const newName = name.toUpperCase()
+  const url = 'https://via.placeholder.com/150'
+
+  function sum(a, b){
+    return a+b
+  }
+  return (
+    <div className="App">
+      <h2>Alterando JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma {sum(1, 4)}</p>
+      <img src={url} alt='minha imagem' />
+      <p> oii!!</p>
+    </div>
+  )
+}
+
+export default App;
