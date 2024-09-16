58f4822bc691c217fd1ae232db78efb10e51434e
oAmorim2k5 <victorhuski@gmail.com>
2024-09-16 19:37:35 -0300

    Avançando em props - 'tipando'

--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,11 @@ function List() {
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
-                <Item marca="Renault"/>
+                <Item marca="Ferrari" ano_lancamento={2024}/>
+                <Item marca="Fiat" ano_lancamento={1985}/>
+                <Item marca="Renault" ano_lancamento={2007}/>
+                <Item ano_lancamento={400}/>
+                <Item marca="Tesla"/>
             </ul>
         </>
     )
