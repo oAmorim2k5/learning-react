7284d4e618d8013c696993fa39ea0bb88b67e786
oAmorim2k5 <victorhuski@gmail.com>
2024-09-16 19:58:31 -0300

    Trabalhando com eventos - Form.js ; Evento.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
 import List from './components/List'
+import Evento from './components/Evento'
+import Form from './components/Form'
 
 function App() {
 
@@ -10,18 +12,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>OiOiOiOi</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome={nome}/>
-      <SayMyName nome="Victor"/>
-      <Pessoa 
-      nome="Beatriz" 
-      idade="18" 
-      profissao="Kyndryl"
-      foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   )
 }
