7175628d072f64b114717baeed54772f168fe6f2
oAmorim2k5 <victorhuski@gmail.com>
2024-09-21 09:52:05 -0300

    Trabalhando com eventos em props - Evento.js ; Button.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1>Testando eventos</h1>
-      <Evento numero="1" />
-      <Evento numero="2" />
+      <Evento />
       <Form />
     </div>
   )
